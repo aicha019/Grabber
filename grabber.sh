62b4d18e14e37d452a7aaa3d6cbb177d6c876e42
aicha019 <aichafofana019@gmail.com>
2025-11-20 19:53:52 +0100

    exo global.log mise en forme psk l'autre ct un flop

--- grabber.sh
+++ grabber.sh
@@ -2,30 +2,28 @@
 
 dir='/home/aichac/workspace/grabber'
 
-echo $(date +\%d/\%m/\%Y) >> $dir/output/global.log
+> $dir/output/global.log
+
+echo $(date +%d.%m.%Y) >> $dir/output/global.log
 
 echo "---HOSTNAME---" >> $dir/output/global.log
-hostnamectl | grep "Static hostname" | cut -d: -f2 | xargs>> $dir/output/global.log
+hostname | cut -d"." -f1 >> $dir/output/global.log
 
 echo "---BIOS VERSION---" >> $dir/output/global.log
-sudo dmidecode -t bios | head -n 5 >> $dir/output/global.log
+sudo dmidecode -t bios | grep -E "Vendor|Version|Release" | head -n3 >> $dir/output/global.log
 
 
-echo "---INFOS CPU---" >> $dir/output/global.log
-inxi -C | head -n 1 >>$dir/output/global.log
+echo "---INFO CPU---" >> $dir/output/global.log
+inxi -C | head -n1 | cut -d: -f2 | xargs >> $dir/output/global.log
 
-echo "---KERNEL INFO---" >> $dir/output/global.log
-uname -mrs >> $dir/output/global.log
+echo "---KERNEL INFOS---" >> $dir/output/global.log
+uname -mr >> $dir/output/global.log
 
 echo "---MEMOIRE---" >> $dir/output/global.log
-free -h >> $dir/output/global.log
+free -h | grep "Mem:" >> $dir/output/global.log
 
-echo "---DISKS---" >> $dir/output/global.log
-lsblk | head -n 10 >> $dir/output/global.log
-df -h | grep  "dev/sda1" >> $dir/output/global.log
+echo "---DISK---" >> $dir/output/global.log
+df -h | grep "/dev/sda1" >> $dir/output/global.log
 
-echo "--- CPU temp---" >> $dir/output/global.log
-sensors | grep Package | head -n 3 >> $dir/output/global.log
+echo "---END---" >> $dir/output/global.log
 
-
-echo "--- END---" >> $dir/output/global.log
