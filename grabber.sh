20aa6a689cf9c8ff69403c801d4d94e64b9c639c
aicha019 <aichafofana019@gmail.com>
2025-11-27 19:23:22 +0100

    devoir grabber sur l'espace entre les --- ---

--- grabber.sh
+++ grabber.sh
@@ -4,24 +4,27 @@ dir='/home/aichac/workspace/grabber'
 
 echo $(date +%d.%m.%Y) >> $dir/output/global.log
 
-echo "---HOSTNAME---" >> $dir/output/global.log
+echo "---- HOSTNAME ----" >> $dir/output/global.log
 hostname | cut -d"." -f1 >> $dir/output/global.log
 
-echo "---BIOS VERSION---" >> $dir/output/global.log
-sudo dmidecode -t bios | grep -E "Vendor|Version|Release" | head -n3 >> $dir/output/global.log
+echo "---- BIOS VERSION ----" >> $dir/output/global.log
+sudo dmidecode -t bios | grep Version | head -n3 >> $dir/output/global.log
 
 
-echo "---INFO CPU---" >> $dir/output/global.log
-inxi -C | head -n1 | cut -d: -f2 | xargs >> $dir/output/global.log
+echo "---- INFO CPU ----" >> $dir/output/global.log
+inxi -c | grep CPU >> $dir/output/global.log
 
-echo "---KERNEL INFOS---" >> $dir/output/global.log
+echo "---- KERNEL INFOS ----" >> $dir/output/global.log
 uname -mr >> $dir/output/global.log
 
-echo "---MEMOIRE---" >> $dir/output/global.log
-free -h | grep "Mem:" >> $dir/output/global.log
 
-echo "---DISK---" >> $dir/output/global.log
-df -h | grep "/dev/sda1" >> $dir/output/global.log
+echo "---- INFOS ----" >> $dir/output/global.log
+free -h >> $dir/output/global.log
+lsblk >> $dir/output/global.log
+df -h | grep /dev/sda1 >> $dir/output/global.log
 
-echo "---END---" >> $dir/output/global.log
+echo "---- CPU Temp ----" >> $dir/output/global.log
+sensors | grep Package >> $dir/output/global.log
+
+echo "---- END ----" >> $dir/output/global.log
 
