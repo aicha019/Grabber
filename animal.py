5bfae75410367b39561cd0eba3053ee206be9332
aicha019 <aichafofana019@gmail.com>
2026-01-08 16:52:45 +0100

    dns ordi

---
+++ animal.py
@@ -0,0 +1,8 @@
+class Chien():
+    nb_pattes=4 
+    pelage="brun"
+    race="rotweiler"
+    pos=0
+
+    def walk(self):
+        self.pos+=1
---
+++ ordinateur.py
@@ -0,0 +1,43 @@
+class Ordi():
+    CPU=" "
+    CPU_CORES_NUMBER=" "
+    CPU_THREADS_NUMBER=" "
+    CPU_FREQUENCY_MIN=" "
+    CPU_FREQUENCY_MAX=" "
+    CPU_FREQUENCY_CUR=" "
+    MB_SERIAL=" " 
+    GPU_MODEL=" "
+    GPU_MEMORY=" "
+    RAM=" "
+    RAM_NUMBER=" "
+    RAM_SLOTS_NUMBER=" "
+    RAM_O_SIZE=" "
+    RAM_O_FREQUENCE=" "
+    RAM_O_SLOTS=" "
+
+    DISK=" "
+    DISK_NUMBER=" "
+    DISK1_SIZE=" "
+    DISK1_USED=" "
+    DISK1_AVAILABLE=" "
+    DISK1_PARTS=" "
+    DISK1_PART3_TYPE=" "
+    DISK1_PART3_SIZE=" "
+    OS=" "
+    KERNEL=" "
+
+
+
+    def fletch_summary(self):
+        return
+
+    def shutdown():
+        return
+    def get_status(self):
+        return
+    def link_to_user(self_user):
+        return
+    def remove_user(self, user):
+        return
+    def show_users(self):
+        return
