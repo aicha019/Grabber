dc05d69cf402f077caca6126457f479002fa7097
aicha019 <aichafofana019@gmail.com>
2026-02-12 09:59:24 +0100

    ajout if mac adress exist bla bla bla

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
 from fastapi import FastAPI, Request, HTTPException
-import json
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from sqlmodel import SQLModel, create_engine, Session, select
+import json 
+
+from models import Ordinateur
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+sqlite_url = f"sqlite:///database.db"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+SQLModel.metadata.create_all(engine)
+
+
+#ordi1 = Ordinateur()
+
+@app.get("/ordi/{ordi_id}", response_class=HTMLResponse)
+async def get_ordi1_info(request: Request, ordi_id: int):
+    with Session(engine) as session: 
+        statement = select(Ordinateur).where( Ordinateur.id == ordi_id )
+        this_ordi = session.exec(statement).first() 
+        return templates.TemplateResponse(
+            request=request, name="ordi.html", context={"ordi": this_ordi}
+    )
+
+@app.get("/ordi/{ordi_id}/delete")
+async def delete_ordi(request: Request, ordi_id: int):
+    
 
 @app.post("/endpoint")
 async def receive_info(request: Request):
@@ -17,4 +49,43 @@ async def receive_info(request: Request):
     
     # Debug
     print("Infos recues :", data)
+    hardware = data.get("HARDWARE", {})
+    
+    with Session(engine) as session:
+        statement = select(Ordinateur).where( Ordinateur.mac_adress == hardware.get('mac_address', ""))
+        ordi_exists = session.exec(statement).first()
+
+    if ordi_exists is None:
+        ordi1 = Ordinateur()
+    ordi1.hostname = hardware.get("hostname", "chépa")
+    ordi1.cpu_cores_number = hardware.get('cpu_cores_number', "")
+    ordi1.cpu_threads_number = hardware.get('cpu_threads_number', "")
+    ordi1.cpu_frequency_min = hardware.get('cpu_frequency_min', "")
+    ordi1.cpu_frequency_max = hardware.get('cpu_frequency_max', "")
+    ordi1.cpu_frequency_cur = hardware.get('cpu_frequency_cur', "")
+
+    ordi1.gpu_model = hardware.get('gpu_model', "")
+    ordi1.gpu_memory = hardware.get('gpu_memory', "")
+    ordi1.mac_adress = hardware.get('mac_adress', "")
+    ordi1.mb_serial = hardware.get('mb_serial', "")
+
+    ordi1.ram_size = hardware.get('ram_size', "")
+    ordi1.ram_number = hardware.get('ram_number', "")
+    ordi1.ram_slots_number = hardware.get('ram_slots_number', "")
+
+    #ordi1.ram_0_size = hardware.get('ram_0_size', "")
+    ordi1.ram_0_frequence = hardware.get('ram_0_frequence', "")
+    ordi1.ram_0_slots = hardware.get('ram_0_slots', "")
+
+    software = data.get("SOFTWARE", {})
+    ordi1.kernel = software.get('kernel', "")
+    print(f"Le hostname de l'ordi est {ordi1.hostname}")
+    print(f"Le serial de la mb est {ordi1.mb_serial}")
+
+    session = Session(engine)
+    session.add(ordi1)
+    session.commit()
+
     return {"status": "ok"}
+
+
